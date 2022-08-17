43be5107adc47c0bee39987080076b5b2c2f0679
Lucas Arrabal <Lucas Arrabal@DESKTOP-J0CMM99>
2022-08-17 15:56:36 -0300

    Add project files.

---
+++ MVC/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Database = MVC; Integrated Security=true");
+        }
+    }
+}
